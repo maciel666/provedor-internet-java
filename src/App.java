87e36f6a540cbfdb4f2ccead6a249cb5111aeb22
marilaine maciel coraleski <marilainemaciel66@gmail.com>
2024-04-08 19:56:55 -0300

    resolução de problema na entradsa de texto

--- src/App.java
+++ src/App.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, bem-vindo a central de atendimento {provedor}");
-        
-        System.out.printf("olá,bem-vindo a central de atendimentio %s"); 
 
-        System.out.println("\nMeu nome é {atendente}, sou seu assistente virtual");
 
-        System.out.println("\nDurante o atendimento siga minhas instruções");
 
-        System.out.println("\nPrimeiramente informe seu nome");
+        // 1- imprimir a informação dp proverdor 
 
         System.out.println("Obrigado {nome}, como posso lhe ajudar?");
+        System.out.printf("Olá, bem-vindo a central de atendimento %s",provedor);  
+    
+ 
+ 
+        String atendente = "Bia";
+     
+        System.out.printf("\nMeu nome é %s, sou seu assistente virtual %s ", atendente, "ok");
+ 
+ 
+        System.out.printf("\nDurante o atendimento siga minhas instruções");
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.print("\nPrimeiramente informe seu nome ");
+        String nome = scanner.next();
+
+        Scanner scanner = new Scanner (System.in);
+        String nome = scanner.nextLine();
+ 
+ 
+        System.out.printf("\nObrigado %s, como posso lhe ajudar?", nome );
+        String resposta = scanner.nextLine();
+        // imprimindo texto concatenado 
+        System.out.println("sua resposta foi: " + resposta);
+        scanner.close();
 
     }
 }
